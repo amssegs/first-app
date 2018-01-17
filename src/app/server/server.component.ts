4f17fb6e7570c523b21d3b776534a5bdb875f017
amssegs <amssegs@gmail.com>
2018-01-17 11:40:43 -0500

    Directives

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
  selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  styles: [`
+    .online {
+      color: white;
+    }
+    `]
 })
-export class ServerComponent implements OnInit {
+export class ServerComponent {
 
-  constructor() { }
+  serverId;
+  serverStatus = 'Offline';
 
-  ngOnInit() {
+  getServerStatus() {
+    return this.serverStatus;
   }
+
+  getServerId() {
+    return this.serverId;
+  }
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
+  constructor() {
+    this.serverId = Math.round(Math.random() * 10);
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  // ngOnInit() {
+  // }
+
+
 
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,17 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  // selector: 'app-servers',
-  selector: '.app-servers',
+   selector: 'app-servers',
+ // selector: '[app-servers]',
   // selector: '[app-servers]',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = '';
+  // serverCreationStatus = 'No Server was created';
+  serverName = 'TestServer';
+  servers = ['test1', 'test2'];
+  constructor() {
+    // setTimeout(
+    //   () => {
+    //     this.allowNewServer = true;
+    //   }, 2000
+    // );
+  }
 
   ngOnInit() {
+  }
+
+  onClickEvent() {
+    this.serverCreationStatus = this.serverCreationStatus + '\n Server Created with name ' + this.serverName;
+    this.servers.push(this.serverName);
+    this.allowNewServer = true;
+    // this.serverName = '';
+
+  }
+  onServerCreate(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
